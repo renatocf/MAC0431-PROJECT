a0d11d75f4c36d30352ae80b671e22082a6b302b
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2015-10-30 06:25:36 -0200

    Add input reading

--- src/waves.cpp
+++ src/waves.cpp
@@ -15,14 +15,52 @@
 /******************************************************************************/
 
 // Standard headers
+#include <array>
 #include <cstdlib>
 #include <iostream>
+
+template<std::size_t N>
+using Dimension = std::array<unsigned int, N>;
+
+template<std::size_t N>
+std::istream &operator>>(std::istream &input, Dimension<N> &dim) {
+  if (dim.empty()) return input;
+
+  char lpar, rpar, comma;
+  if (dim.size() != 1) input >> lpar;
+
+  input >> dim[0];
+  for (unsigned int i = 1; i < dim.size(); i++) {
+    input >> dim[i];
+    input >> comma;
+  }
+
+  if (dim.size() != 1) input >> rpar;
+
+  return input;
+}
 
 int main(int argc, char **argv) {
   if (argc != 3) {
     std::cerr << "USAGE: " << argv[0] << " param_file num_procs" << std::endl;
     return EXIT_FAILURE;
   }
+
+  Dimension<2> lake_dimensions;
+  Dimension<2> matrix_dimensions;
+  unsigned int time;
+  double speed;
+  unsigned int num_iterations;
+  double drop_probability;
+  unsigned int seed;
+
+  std::cin >> lake_dimensions;
+  std::cin >> matrix_dimensions;
+  std::cin >> time;
+  std::cin >> speed;
+  std::cin >> num_iterations;
+  std::cin >> drop_probability;
+  std::cin >> seed;
 
   return EXIT_SUCCESS;
 }
